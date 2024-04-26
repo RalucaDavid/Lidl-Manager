012cdb02a5b60f979a06f4f812d516a43cd4f55e
RalucaDavid <117584603+RalucaDavid@users.noreply.github.com>
2024-04-26 23:49:48 +0300

    Create the project.

---
+++ LidlManager/LidlManager/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LidlManager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
